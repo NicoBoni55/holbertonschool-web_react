1745ed7cc1edd5ae6cb1f5a9fe2f1a604cc8708b
NicoBoni55 <bonifrank55@gmail.com>
2025-04-19 13:24:16 -0300

    add image-webpack-loader to the webpack config

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -26,6 +26,9 @@ module.exports = {
                             outputPath: './assets/',
                         },
                     },
+                    {
+                        loader: 'image-webpack-loader',
+                    }
                 ],
             }
         ]
