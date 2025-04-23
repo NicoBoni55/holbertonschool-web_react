231580b8119956e990626912eed06e830aa970ba
NicoBoni55 <bonifrank55@gmail.com>
2025-04-23 13:23:40 -0300

    fix notification component

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -5,7 +5,10 @@ function Notifications() {
     return (
         <div className="notifications">
             <p>Here is the list of notifications</p>
-            <button aria-label='Close' onClick={console.log('Close button has been clicked')}><img src={closeIcon} alt='close-icon'></img></button>
+            <button
+                aria-label='Close' 
+                onClick={() => console.log('Close button has been clicked')}>
+                    <img src={closeIcon} alt='close-icon'></img></button>
             <ul>
                 <li data-priority="1">New course available</li>
                 <li data-priority="2">New resume available</li>
