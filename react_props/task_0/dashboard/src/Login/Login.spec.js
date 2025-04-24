a31ff6c7a817940e107d60f1c9fcc3440295c703
NicoBoni55 <bonifrank55@gmail.com>
2025-04-24 18:50:42 -0300

    test header, footer and login

--- react_props/task_0/dashboard/src/Login/Login.spec.js
+++ react_props/task_0/dashboard/src/Login/Login.spec.js
@@ -1,16 +1,27 @@
 import {screen, render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 
-describe('Login component', () => {
-    it('renders the component', () => {
-        render(<Login />);
-        const p1 = screen.getByText('Login to access the full dashboard');
-        const emailInput = screen.getByPlaceholderText('Email');
-        const passwordInput = screen.getByPlaceholderText('Password');
-        const loginButton = screen.getByText('OK');
-        expect(emailInput).toBeInTheDocument();
-        expect(passwordInput).toBeInTheDocument();
-        expect(loginButton).toBeInTheDocument();
-        expect(p1).toBeInTheDocument();
-    });
-})
+test ('renders the component', async () => {
+    render(<Login />);
+
+    const user = userEvent.setup();
+    const p1 = screen.getByText('Login to access the full dashboard');
+    const emailInput = screen.getByPlaceholderText('Email');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    const emailLabel = screen.getByText('Email');
+    const passwordLabel = screen.getByText('Password');
+    const loginButton = screen.getByText('OK');
+
+    await user.click(emailLabel);
+    await user.click(passwordLabel);
+
+    expect(p1).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(emailLabel).toBeInTheDocument();
+    expect(emailInput).toHaveFocus();
+    expect(passwordInput).toHaveFocus();
+    expect(passwordLabel).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+});
