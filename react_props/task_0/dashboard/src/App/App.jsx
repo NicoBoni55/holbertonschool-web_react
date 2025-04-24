cf98b1edab03a3470b7cdff298e7217499fdd856
NicoBoni55 <bonifrank55@gmail.com>
2025-04-24 18:33:30 -0300

    add App.css

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -2,6 +2,7 @@ import Notifications from '../Notifications/Notifications'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Login from '../Login/Login'
+import './App.css'
 
 function App() {
 
