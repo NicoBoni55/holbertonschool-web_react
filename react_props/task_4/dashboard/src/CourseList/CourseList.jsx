1acbfeea818ce75ea01c760e43fbc4b04a0fd60f
NicoBoni55 <bonifrank55@gmail.com>
2025-05-02 11:43:31 -0300

    fix CourseList

--- react_props/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -3,9 +3,10 @@ import CourseListRow from "./CourseListRow"
 export default function CourseList(courses=[]) {
     return (
         <table id="CourseList">
-            <thead></thead>
-            <CourseListRow isHeader={true} textFirstCell={"Available courses"} />
-            <CourseListRow isHeader={true} textFirstCell={"Course name"} textSecondCell={"Credit"} />
+            <thead>
+                <CourseListRow isHeader={true} textFirstCell={"Available courses"} />
+                <CourseListRow isHeader={true} textFirstCell={"Course name"} textSecondCell={"Credit"} />
+            </thead>
             <tbody>
                 {courses.length === 0 ? (
                     <CourseListRow isHeader={false} textFirstCell={"No course available yet"} textSecondCell={null} />
