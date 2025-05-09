eebf8c1cb1ed27f031adf9f563e908e15ff1b598
NicoBoni55 <bonifrank55@gmail.com>
2025-05-09 16:44:41 -0300

    fix displayname

--- react_component/task_4/dashboard/src/HOC/WithLogging.jsx
+++ react_component/task_4/dashboard/src/HOC/WithLogging.jsx
@@ -3,17 +3,17 @@ import React, {Component} from "react";
 function WithLogging(WrappedComponent) {
     class HOC extends Component {
         componentDidMount() {
-            console.log(`Component ${WrappedComponent.name} is mounted`);
+            console.log(`Component ${WrappedComponent.name || 'Component'} is mounted`);
         }
         componentWillUnmount() {
-            console.log(`Component ${WrappedComponent.name} is going to unmount`);
+            console.log(`Component ${WrappedComponent.name || 'Component'} is going to unmount`);
         }
         render() {
             return <WrappedComponent {...this.props} />;
         }
     }
 
-    HOC.displayName = `WithLogging(${WrappedComponent.name})`;
+    HOC.displayName = `WithLogging(${WrappedComponent.name || 'Component'})`;
     return HOC;
 }
 
