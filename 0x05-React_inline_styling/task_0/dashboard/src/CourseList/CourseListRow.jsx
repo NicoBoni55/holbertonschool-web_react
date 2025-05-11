f2a7f1f0540282f37b52d3d13ee563be7ca9d235
NicoBoni55 <bonifrank55@gmail.com>
2025-05-11 15:51:09 -0300

    add const styles

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.jsx
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,6 +1,8 @@
 export default function CourseListRow({ isHeader=false, textFirstCell="", textSecondCell=null }) {
-    return (
-        <tr style={isHeader ? {backgroundColor: "#deb5b545"}: {backgroundColor: "#f5f5f5ab"}}>
+    const HeaderStyle = {backgroundColor: '#deb5b545'};
+    const rowStyle = {backgroundColor: '#f5f5f5ab'};
+    return (    
+        <tr style={isHeader ? HeaderStyle : rowStyle}>
            {isHeader ? 
                ( textSecondCell === null ? (
                    <th colSpan={2}>{textFirstCell}</th>
