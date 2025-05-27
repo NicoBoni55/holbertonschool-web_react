41323140fa4a0cd1f4ed9ce79ae8366fb3da2123
NicoBoni55 <bonifrank55@gmail.com>
2025-05-27 11:07:01 -0300

    Login controlled components

---
+++ react_state/task_2/dashboard/src/Login/Login.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import PropTypes from 'prop-types';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      enableSubmit: false,
+    };
+  }
+
+  handleLoginSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    this.props.logIn(this.state.email, this.state.password);
+  };
+
+  handleChangeEmail = (e) => {
+    const email = e.target.value;
+    this.setState({ email}, () => {
+      this.validateForm();
+    })
+  };
+
+  handleChangePassword = (e) => {
+    const password = e.target.value;
+    this.setState({ password }, () => {
+      this.validateForm();
+    });
+  };
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    const validEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const validPassword = password.length >= 8;
+    this.setState({ enableSubmit: validEmail && validPassword });
+  }
+
+  render() {
+    const {email, password, enableSubmit} = this.state;
+
+    return (
+      <div className={css(styles.body)}>
+        <p>Login to access the full dashboard</p>
+        <form onSubmit={this.handleLoginSubmit}>
+          <div className={css(styles.inputGroup)}>
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              type="email"
+              className={css(styles.input)}
+              value={email}
+              onChange={this.handleChangeEmail}
+            />
+          </div>
+          <div className={css(styles.inputGroup)}>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              className={css(styles.input)}
+              value={password}
+              onChange={this.handleChangePassword}
+            />
+          </div>
+          <div className={css(styles.buttonWrapper)}>
+            <input 
+            type="submit" 
+            value="OK" 
+            className={css(styles.button)}
+            disabled={!enableSubmit} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  body: {
+    padding: "30px",
+    "@media (max-width: 900px)": {
+      padding: "20px",
+    },
+  },
+  inputGroup: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: "1em",
+    "@media (max-width: 900px)": {
+      flexDirection: "column",
+      alignItems: "flex-start",
+    },
+  },
+  input: {
+    marginLeft: "10px",
+    "@media (max-width: 900px)": {
+      marginLeft: "0",
+      marginTop: "5px",
+      width: "100%",
+    },
+  },
+  buttonWrapper: {
+    "@media (max-width: 900px)": {
+      display: "flex",
+      justifyContent: "flex-start",
+    },
+  },
+  button: {
+    marginLeft: "10px",
+    "@media (max-width: 900px)": {
+      marginLeft: "0",
+    },
+  },
+});
+
+Login.propTypes = {
+  logIn: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool,
+};
+
+Login.defaultProps = {
+  logIn: () => {},
+  isLoggedIn: false,
+};
+
+export default Login;
---
+++ react_state/task_2/dashboard/src/Login/Login.spec.js
@@ -0,0 +1,89 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Login from "./Login";
+import { StyleSheetTestUtils } from 'aphrodite';
+
+beforeAll(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterAll(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+
+test('the text content within the 2 p elements in the app-body and app-footer divs matches', () => {
+  render(<Login />);
+  const divbody = screen.getByText(/Login to access the full dashboard/i);
+
+  expect(divbody).toBeInTheDocument();
+});
+
+test('renders 2 input elements', () => {
+  render(<Login />);
+  const labelemail = screen.getByLabelText(/Email/i);
+  const labelpassword = screen.getByLabelText(/Password/i);
+
+  expect(labelemail).toBeInTheDocument();
+  expect(labelpassword).toBeInTheDocument();
+});
+
+test('renders 2 label elements with the text Email and Password', () => {
+  render(<Login />);
+  const labelemail = screen.getByLabelText(/email/i);
+  const labelpassword = screen.getByLabelText(/password/i);
+
+  expect(labelemail).toBeInTheDocument();
+  expect(labelpassword).toBeInTheDocument();
+});
+
+test('renders a button with the text OK', () => {
+  render(<Login />);
+  const button = screen.getByRole('button', { name: /ok/i });
+
+  expect(button).toBeInTheDocument();
+});
+
+test('button is disabled by default', () => {
+  render(<Login />);
+  const button = screen.getByRole('button', { name: /ok/i });
+
+  expect(button).toBeDisabled();
+});
+
+test('button is enabled when email and password are valid', () => {
+  render(<Login />);
+  const emailInput = screen.getByLabelText(/email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const button = screen.getByRole('button', { name: /ok/i });
+
+  fireEvent.change(emailInput, { target: { value: ''}});
+  fireEvent.change(passwordInput, { target: { value: '' }});
+  expect(button).toBeDisabled();
+
+  fireEvent.change(emailInput, { target: { value: 'hola' }});
+  fireEvent.change(passwordInput, { target: { value: '12345678' }});
+  expect(button).toBeDisabled();
+
+  fireEvent.change(emailInput, { target: { value: 'example@gmail.com' }});
+  fireEvent.change(passwordInput, { target: { value: '1234' }});
+  expect(button).toBeDisabled();
+
+  fireEvent.change(emailInput, { target: { value: 'example@gmail.com' }});
+  fireEvent.change(passwordInput, { target: { value: '12345678' }});
+  expect(button).toBeEnabled();
+});
+
+test('logIn method is correctly called with the user´s email and password', () => {
+  const logInMock = jest.fn();
+  render(<Login logIn={logInMock} />);
+  
+  const emailInput = screen.getByLabelText(/email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const button = screen.getByRole('button', { name: /ok/i });
+
+  fireEvent.change(emailInput, { target: { value: 'nico@gmail.com' } });
+  fireEvent.change(passwordInput, { target: { value: '12345678' } });
+  fireEvent.click(button);
+  expect(logInMock).toHaveBeenCalledWith(emailInput.value, passwordInput.value);
+  expect(logInMock).toHaveBeenCalledTimes(1);
+});
